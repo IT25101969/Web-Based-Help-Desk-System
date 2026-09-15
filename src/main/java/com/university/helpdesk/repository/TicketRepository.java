4ef156000b13ccdd1004cbb8f58685e8d305c9b1
Sachin Jayathilaka <it25101969@my.sliit.lk>
2026-09-15 06:51:50 +0530

    Add department scoped ticket query

--- src/main/java/com/university/helpdesk/repository/TicketRepository.java
+++ src/main/java/com/university/helpdesk/repository/TicketRepository.java
@@ -4,10 +4,12 @@ import com.university.helpdesk.entity.Ticket;
 import com.university.helpdesk.entity.TicketStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     List<Ticket> findByStudentUserIdOrderByCreatedDateDesc(Long userId);
     List<Ticket> findAllByOrderByCreatedDateDesc();
     long countByStatus(TicketStatus status);
+    List<Ticket> findByCategoryDepartmentDepartmentIdInOrderByCreatedDateDesc(Collection<Long> departmentIds);
 }
