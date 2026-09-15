7e776e8ebd19923cfbca5c768a0a8d2b19ecccf0
Sachin Jayathilaka <it25101969@my.sliit.lk>
2026-09-15 06:49:56 +0530

    Extend CategoryRepository.java

--- src/main/java/com/university/helpdesk/repository/CategoryRepository.java
+++ src/main/java/com/university/helpdesk/repository/CategoryRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByStatusIgnoreCaseOrderByCategoryNameAsc(String status);
+    java.util.Optional<Category> findByCategoryName(String categoryName);
 }
