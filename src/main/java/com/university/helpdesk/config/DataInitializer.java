fde45b2bcf82c7510991dcf8cc7ec32b749c9b03
Sachin Jayathilaka <it25101969@my.sliit.lk>
2026-09-15 08:04:21 +0530

    Order core demo data before dependent seed data

--- src/main/java/com/university/helpdesk/config/DataInitializer.java
+++ src/main/java/com/university/helpdesk/config/DataInitializer.java
@@ -11,12 +11,14 @@ import com.university.helpdesk.repository.UserRoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class DataInitializer {
 
     @Bean
+    @Order(1)
     CommandLineRunner initializeData(
             UserAccountRepository userAccountRepository,
             RoleRepository roleRepository,
