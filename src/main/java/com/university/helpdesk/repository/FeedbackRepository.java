a7e716e32bacf49a86ae33b538f1184d8486fcc5
Sachin Jayathilaka <it25101969@my.sliit.lk>
2026-09-15 06:57:06 +0530

    Add FeedbackRepository feedback flow

---
+++ src/main/java/com/university/helpdesk/repository/FeedbackRepository.java
@@ -0,0 +1,11 @@
+package com.university.helpdesk.repository;
+
+import com.university.helpdesk.entity.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    Optional<Feedback> findByTicketTicketId(Long ticketId);
+    boolean existsByTicketTicketId(Long ticketId);
+}
