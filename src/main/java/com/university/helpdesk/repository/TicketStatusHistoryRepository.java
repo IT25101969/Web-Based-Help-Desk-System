e5914a602a4b9ce7169e9a28b8fc0d97ab95f209
Sachin Jayathilaka <it25101969@my.sliit.lk>
2026-09-15 06:48:17 +0530

    Add repository for TicketStatusHistory

---
+++ src/main/java/com/university/helpdesk/repository/TicketStatusHistoryRepository.java
@@ -0,0 +1,10 @@
+package com.university.helpdesk.repository;
+
+import com.university.helpdesk.entity.TicketStatusHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TicketStatusHistoryRepository extends JpaRepository<TicketStatusHistory, Long> {
+    List<TicketStatusHistory> findByTicketTicketIdOrderByChangedDateAsc(Long ticketId);
+}
