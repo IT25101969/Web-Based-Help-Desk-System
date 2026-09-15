427a7945586e60261ccbae493e64eee21b56ad95
Sachin Jayathilaka <it25101969@my.sliit.lk>
2026-09-15 06:47:32 +0530

    Add repository for Category

---
+++ src/main/java/com/university/helpdesk/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.university.helpdesk.repository;
+
+import com.university.helpdesk.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findByStatusIgnoreCaseOrderByCategoryNameAsc(String status);
+}
