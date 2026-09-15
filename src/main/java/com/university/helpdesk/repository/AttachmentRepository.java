dc5562bdd9789e29ee30dfa7d3f67005b6f48eee
Sachin Jayathilaka <it25101969@my.sliit.lk>
2026-09-15 06:48:01 +0530

    Add repository for Attachment

---
+++ src/main/java/com/university/helpdesk/repository/AttachmentRepository.java
@@ -0,0 +1,10 @@
+package com.university.helpdesk.repository;
+
+import com.university.helpdesk.entity.Attachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+    List<Attachment> findByTicketTicketIdOrderByUploadedDateAsc(Long ticketId);
+}
